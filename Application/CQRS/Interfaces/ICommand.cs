3574284bc81d5a907bf43702ed33c668d708d85a
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-09-13 20:09:18 -0500

    Moved Commands and Queries

---
+++ Application/CQRS/Interfaces/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Abstract;
+using MediatR;
+
+namespace Application.CQRS
+{
+    public interface ICommand : IRequest<Result>, ICommandBase
+    {
+    }
+
+    public interface ICommand<TResponse> : IRequest<Result<TResponse>>, ICommandBase
+    {
+    }
+
+    public interface ICommandBase
+    {
+    }
+}
