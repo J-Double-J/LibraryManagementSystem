5df83d0eef888db16ae90408f47f6e9ee2072605
Joshua Jacobs <joshua.jacobs126@gmail.com>
2023-09-20 08:10:57 -0500

    Added Patron and its validator

---
+++ Domain/Entities/Patron/Patron.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Abstract;
+using Domain.Primitives;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Patron
+{
+    public class Patron : Entity
+    {
+        public static int MINIMUM_AGE = 4;
+        private const int ADULT_AGE_CUTOFF = 18;
+
+        private Patron(Guid guid, string firstName, string lastName, int age)
+            : base(guid)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        [NotMapped]
+        public bool IsAdult { get { return Age >= ADULT_AGE_CUTOFF; } }
+
+        public static async Task<Result<Patron>> Create(Guid guid, string firstName, string lastName, int age)
+        {
+            Patron patron = new(guid, firstName, lastName, age);
+
+            PatronValidator validator = new();
+
+            DomainValidationResult<Patron> validationResult = await validator.ValidateAsyncGetDomainResult(patron);
+
+            return validationResult;
+        }
+    }
+}
---
+++ Domain/Entities/Patron/PatronValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain.CustomFluentValidation;
+using FluentValidation;
+
+namespace Domain.Entities.Patron
+{
+    public class PatronValidator : LibraryValidator<Patron>
+    {
+        public PatronValidator()
+            : base(LibraryValidatorType.Entity)
+        {
+            RuleFor(patron => patron.FirstName).NotNull().NotEmpty().MaximumLength(50);
+            RuleFor(patron => patron.LastName).NotNull().NotEmpty().MaximumLength(50);
+            RuleFor(patron => patron.Age).GreaterThanOrEqualTo(Patron.MINIMUM_AGE).LessThan(256);
+        }
+    }
+}
